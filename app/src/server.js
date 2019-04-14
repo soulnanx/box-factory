6b43ea5f8e6cfd44da1a1c158a40715bdb48a241
Renan <rsantos.dev@gmail.com>
2019-04-14 13:52:08 -0300

    configuraçao de pastas

--- app/src/server.js
+++ app/src/server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const mongo = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+
+io.on("connection", socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+})
 
 var mongoaddr = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':27017/api';
-console.log(mongoaddr);
 mongo.connect(mongoaddr);
 
+app.use((req, res) => {
+    req.io = io;
+
+    return next();
+})
+
+app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded( {extended: true }) );
-app.use(require("./routes"));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require("./routes"));
 
 app.listen(3333);
